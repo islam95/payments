a6904ff25d4f185241751337697e1dfc30366380
hiwotbible <hiwotbible@gmail.com>
2018-06-05 19:26:22 +0100

    add payment list

--- src/components/Payments.js
+++ src/components/Payments.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Button from "../components/Button";
 import "./Payments.css";
-
+import payments from "../data/payments.js";
+console.log (payments)
 function Payments(props) {
   return (
     <table className="Payments">
@@ -16,33 +17,19 @@ function Payments(props) {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>2018-05-12</td>
-          <td>GBP</td>
-          <td>6.89</td>
-          <td>Dinner with friends at a local restaurant</td>
-          <td>Pending</td>
-          <td>
-            <Button>Cancel</Button>
-          </td>
-        </tr>
-        <tr>
-          <td>2018-02-24</td>
-          <td>USD</td>
-          <td>12.23</td>
-          <td>New headphones purchased from Amazon with free delivery</td>
-          <td>Complete</td>
-          <td />
-        </tr>
-        <tr>
-          <td>2017-12-30</td>
-          <td>AUD</td>
-          <td>28.74</td>
-          <td>Groceries for the week</td>
-          <td>Complete</td>
-          <td />
-        </tr>
-      </tbody>
+        {payments.map((payment)=>(<tr>
+          <td>{payment.date}</td>
+          <td>{payment.currency}</td>
+          <td>{payment.amount}</td>
+          
+          <td>{payment.description}</td>
+          <td>{payment.status}</td>
+          <td><Button>Cancel</Button></td>
+         
+        </tr>)
+        )}
+        
+         </tbody>
       <tfoot>
         <tr>
           <td />
